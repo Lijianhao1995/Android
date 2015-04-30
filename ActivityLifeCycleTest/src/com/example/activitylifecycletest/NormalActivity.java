91773002190e79c1af007f3fc643048ca2ddbb92
lijianhao <lijianhao1995@126.com>
2015-04-30 13:21:37 +0800

    1sh edition

---
+++ ActivityLifeCycleTest/src/com/example/activitylifecycletest/NormalActivity.java
@@ -0,0 +1,15 @@
+package com.example.activitylifecycletest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NormalActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.normal_layout);
+		
+	}
+
+}
---
+++ ActivityTest/src/com/example/activitytest/ThirdActivity.java
@@ -0,0 +1,27 @@
+package com.example.activitytest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ThirdActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.third_layout);
+	}
+	/*
+	Button button=(Button) findViewById(R.id.button_3);
+	button.setOnClickListener(new OnClickListener(){
+		public void onClick(View v){
+			Intent intent=
+
+			
+		}
+	});
+	*/
+
+}
